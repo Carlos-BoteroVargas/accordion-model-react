e3ef447f9d8d2ba48fc3dc94075ffac5e0346a89
Carlos Botero-Vargas <cbotero-vargas@outlook.com>
2021-07-15 11:27:58 -0400

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../app.css';
+import Header from './Header';
+import Accordion from "./Accordion";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
